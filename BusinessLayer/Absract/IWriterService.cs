d3978e52bd5d5451be7a58410e679ae2603a8a5c
rasul sönmez <barbaros743@outlook.com>
2022-09-24 17:55:10 +0300

    witer about

--- BusinessLayer/Absract/IWriterService.cs
+++ BusinessLayer/Absract/IWriterService.cs
@@ -10,5 +10,9 @@ namespace BusinessLayer.Absract
     public interface IWriterService
     {
         List<Writer> GetList();
+        void WriterAdd(Writer writer);
+        void WriterDelete(Writer writer);
+        void WriterUpdate(Writer writer);
+        Writer GetById(int id);
     }
 }
